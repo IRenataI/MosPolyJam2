39b0d9ca16cadf9980f96567ab63fab3b60b7697
RottenEagle1337 <djachkovanton1@yandex.ru>
2024-02-06 14:04:41 +0300

    Added iteration 8

--- MosPolyJam2/Assets/Scripts/Dangers/DangerIteration8.cs
+++ MosPolyJam2/Assets/Scripts/Dangers/DangerIteration8.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class DangerIteration8 : BaseDanger
 {
+    [Header("Danger Iteration 8")]
+    [SerializeField] private string loseAnimationName;
+    private Animator anim;
 
+    private void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    protected override void Fail()
+    {
+        if (IsCompleted)
+            return;
+
+        Debug.Log("Fail");
+
+        anim.Play(loseAnimationName);
+
+        Complete();
+        OnComplete?.Invoke(false);
+    }
 }
